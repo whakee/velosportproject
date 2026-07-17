505dba1b3fd1647001d848395e3f5075f98271ae
whakee <s.tymur2000@gmail.com>
2026-07-17 13:01:06 +0300

    catalog bikes

---
+++ catalog.js
@@ -0,0 +1,32 @@
+const tovari = [
+    {
+        name: "dirt jumper",
+        category: "mtb",
+        price: "500$",
+        image: "https://www.airdropbikes.com/cdn/shop/articles/1800-Fade-Bomber-Ltd-Edition-01_2000x.jpg?v=1663079148"
+    },
+    {
+        name: "downhill",
+        category: "mtb",
+        price: "1500$",
+        image: "https://wallpaperaccess.com/full/2150980.jp"
+    },
+    {
+        name: "road bike",
+        category: "road bikes",
+        price: "750$",
+        image: "https://flatironbike.com/wp-content/uploads/2024/06/choosing_a_beginner_road_bicycle.jpg"
+    },
+    {
+        name: "road nike",
+        category: "road bikes",
+        price: "1000$",
+        image: "https://bikerumor.com/wp-content/uploads/2020/04/2021-Giant-TCR-road-bike_new-lightweight-carbon-aero-road-race-bike-of-Team-CCC_photo-by-Chris-Auld.jpg"
+    },
+    {
+        name: "mountain bike",
+        category: "mtb",
+        price: "200$",
+        image: "https://content.westbrookcycles.co.uk/medium/ecd30dca_t213ttcc-f9-side-alma_m50_over.jpg"
+    },
+]
